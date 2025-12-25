bc6c617d8dfda846541e3b493c3a3e058034cc8d
abusayadebu <abusayad743@gmail.com>
2025-12-25 21:32:49 +0600

    phone number and pin number validation added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 
 // login button click and load on the home page
 const loginBtn = document.getElementById("login-btn").addEventListener("click", function (){
-    window.location.href = "home.html"
+    // get the phone value and pin value
+    const phoneNumber = document.getElementById("phone-number").value;
+    const phoneValue = parseInt(phoneNumber);
+    console.log(phoneValue)
+
+    const pinNumber = document.getElementById("pin-number").value;
+    const pinValue = parseInt(pinNumber);
+    console.log(pinValue);
+
+    // validate the pin number and send to the home page--
+    if(phoneValue === 123456789 && pinValue === 1234){
+        window.location.href = "home.html"
+    }
+    else{
+        alert("Wrong credentials. Please try again")
+    }
 })
+// -------------------------------------
