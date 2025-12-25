1816204b1e6ebd759d02af2a332360a2e03121ef
abusayadebu <abusayad743@gmail.com>
2025-12-25 21:08:08 +0600

    home page and content added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+// login button click and load on the home page
+const loginBtn = document.getElementById("login-btn").addEventListener("click", function (){
+    window.location.href = "home.html"
+})
