9514cbc8cde3387b4b01f08a56c9aecc14cd1c5c
abusayadebu <abusayad743@gmail.com>
2025-12-26 00:20:23 +0600

    transaction history section added

--- js/toggle.js
+++ js/toggle.js
@@ -1,9 +1,18 @@
 const cashOutBigBtn = document.getElementById("cash-big-btn").addEventListener("click", function(){
     document.getElementById("cash-out-form").classList.remove("hidden");
     document.getElementById("add-money-form").classList.add("hidden")
+    document.getElementById("transaction-div").classList.add("hidden");
 })
 
 const addMoneyBigBtn = document.getElementById("add-big-btn").addEventListener("click", function(){
     document.getElementById("add-money-form").classList.remove("hidden");
+    document.getElementById("cash-out-form").classList.add("hidden");
+    document.getElementById("transaction-div").classList.add("hidden");
+})
+
+// transaction btn toggle
+const transactionBigBtn = document.getElementById("transaction-btn").addEventListener("click", function(){
+    document.getElementById("transaction-div").classList.remove("hidden");
+    document.getElementById("add-money-form").classList.add("hidden");
     document.getElementById("cash-out-form").classList.add("hidden");
 })
